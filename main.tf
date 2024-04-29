9abb215ff6dfbba9e890dc7b117254cfb003e6ae
Yasuhisa HONDA <nqvno14@gmail.com>
2024-04-29 23:16:50 +0900

    Initialize terraform state on s3

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "nqv-website-tfstate"
+    region  = "ap-northeast-1"
+    key     = "production.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
